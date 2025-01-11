92b4aeabc9853af491ca1a967f4927a42dc1bb7f
Craig Sharp <craigsharp@macbookpro.home>
2025-01-11 12:06:13 +0000

    changed to new sidebar

--- app/dashboard/components/menu-title.tsx
+++ app/dashboard/components/menu-title.tsx
@@ -1,9 +1,12 @@
 import { PersonStandingIcon } from 'lucide-react';
+import Image from 'next/image';
+import Vector from '@/public/Vector.svg';
 
 export default function MenuTitle() {
   return (
     <h4 className="flex items-center">
       <PersonStandingIcon size={40} className="text-primary" /> SupportMe
+      <Image src={Vector} width={40} height={40} alt="helmet" />
     </h4>
   );
 }
